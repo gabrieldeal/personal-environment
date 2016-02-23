e2a494b98fd73b37d99c75e19c1b20bd981fb316
Gabriel M Deal <gabriel@dicksonlabs.com>
2016-02-23 01:47:18 -0800

    Adding a Ruby 2.2.2 and Bundler to Puppet manifest

--- cygwin/vagrant/oss/manifests/default.pp
+++ cygwin/vagrant/oss/manifests/default.pp
@@ -7,11 +7,27 @@ class { 'rvm': }
 rvm::system_user {
   vagrant: ;
 }
+rvm_system_ruby {
+  'ruby-2.2.2':
+    ensure      => 'present',
+    default_use => true;
+}
+rvm_gem {
+  'bundler':
+    name => 'bundler',
+    ruby_version => 'ruby-2.2.2',
+    ensure => latest,
+    require => Rvm_system_ruby['ruby-2.2.2'];
+}
 
 package { 'emacs24':
   ensure => 'installed'
 }
 
 package { 'git':
+  ensure => 'installed'
+}
+
+package { 'libmysqlclient-dev':
   ensure => 'installed'
 }
